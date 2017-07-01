ad90a0ff684dd800545067d97775563d9e3bffd1
oktilon <oktilon@gmail.com>
2017-07-02 02:05:03 +0300

    Removed extra lines

--- MyBooks/Classes.cs
+++ MyBooks/Classes.cs
@@ -1,13 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
-using System.Drawing;
-using System.IO;
-
-using gSet = MyBooks.Properties.Settings;
-using RsRc = MyBooks.Properties.Resources;
 
 namespace MyBooks
 {
